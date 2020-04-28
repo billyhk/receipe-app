7177488cce6554e545268ccc1656206d80f7e86f
billyh <billyhkaufman@outlook.com>
2020-04-28 16:19:16 -0400

    structured show page

--- src/components/RecipeInformation.js
+++ src/components/RecipeInformation.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-
-	/*
+/*
 like a componentDidMount(){}...
 
   	// useEffect(() => {
@@ -19,24 +18,54 @@ like a componentDidMount(){}...
 
   */
 
-
 function RecipeInformation(props) {
-    
-    const url = `${props.searchOptions.api}${props.recipeId}/information?apiKey=${props.searchOptions.key}`;
+	const [thisRecipe, setThisRecipe] = useState('');
 
-	fetch(url)
-		.then((response) => response.json())
-		.then((response) => {
-            console.log('response.title:\n', response.title, '\nresponse.summary\n', response.summary)
-		})
-		.catch(console.error);
+	function getThisRecipe() {
+		const url = `${props.searchOptions.api}${props.recipeId}/information?apiKey=${props.searchOptions.key}`;
+		fetch(url)
+			.then((response) => response.json())
+			.then((response) => {
+				setThisRecipe(response);
+				console.log(response);
+			})
+			.catch(console.error);
+	}
 
+	useEffect(() => {
+		getThisRecipe();
+		// eslint-disable-next-line;
+	}, []);
+
+	if (!thisRecipe) {
+		return 'loading...';
+	}
 	return (
 		<>
-			<h1>Recipe Information</h1>
-			<h2></h2>
+			<img src={thisRecipe.image} />
+			<h2>{thisRecipe.title}</h2>
+
+			{Array.of(thisRecipe.cuisines).map((item, i) => (
+				<p>{item}</p>
+			))}
+
+			<p>
+				<strong>Prep Duration:</strong> {thisRecipe.readyInMinutes} minutes
+			</p>
+            
+			<h4>Ingedients</h4>
+
+			{/* {Array.of(thisRecipe.extendedIngredients).map((item, i) => (
+                <p>{item}</p>
+                ))} */}
+
+			<h4>Instructions</h4>
+			<div dangerouslySetInnerHTML={{ __html: thisRecipe.instructions }} />
 		</>
 	);
 }
 
+/* <h4>Recipe Summary</h4> */
+
+/* <div dangerouslySetInnerHTML={{ __html: thisRecipe.summary }} /> */
 export default RecipeInformation;
